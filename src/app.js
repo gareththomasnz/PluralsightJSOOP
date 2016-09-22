89b85d7f202a308227dffdc8d9e093930cd55a88
gareththomasnz <gareththomasnz@yahoo.com>
2016-09-22 17:59:59 +1200

    fourth commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log("in app.js");
+class Drone{
+     constructor(id, name){
+        this.id = id;
+        this.name = name;      
+        }   
+}
+
+let drone = new Drone('A123', 'Flyer');
+        
+console.log("ID: " + drone.id + " is called " + drone.name);  
+
