393e6f833d4ee49b8b7e03b5ec165dcf4a5a3bd7
gareththomasnz <gareththomasnz@yahoo.com>
2016-09-24 16:49:53 +1200

    fifth commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,55 @@
-class Drone{
-     constructor(id, name){
-        this.id = id;
-        this.name = name;      
-        }   
+class Vehicle{
+     constructor(licenseNum){
+          this.licenseNum = licenseNum;
+          this.gpsEnabled = true;
+     }
 }
 
-let drone = new Drone('A123', 'Flyer');
-        
-console.log("ID: " + drone.id + " is called " + drone.name);  
+class Drone extends Vehicle{
+     //constructor(id, name){
+     //   this._id = id;
+     //   this._name = name;      
+     //   }
+     //   fly(){
+     //       console.log("Drone " + this.id + " is flying");    
+     //   }
+        //get id(){
+        //        return this._id + " TEMPORARY";        
+        //}
+        //set id(){
+        //       this._id = value; 
+        //}
+        //get name(){
+        //       return this._name + " TEMPORARY";   
+        //}
+        //set name(){
+        //      this._name = value;   
+        //}
+}
 
+//Drone.maxHeight = 2000;
+//
+//let drone = new Drone('A123', 'Flyer');
+//let drone2 = new Drone('B456', 'Twirl');
+//
+//drone.maxHeight = 5000;
+//        
+//console.log("ID: " + drone.id + " is called " + drone.name);
+//console.log("ID: " + drone2.id + " is called " + drone2.name);
+//console.log(Drone.maxHeight);
+//console.log(drone.maxHeight);
+//
+//drone.fly();
+//drone2.fly();
+
+class Car extends Vehicle{
+     constructor(licenseNum){
+          super(licenseNum);
+          this.gpsEnabled = false;
+     }     
+}
+
+let c = new Car('A123');
+
+console.log(c.licenseNum);
+console.log(c.gpsEnabled);
